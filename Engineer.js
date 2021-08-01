d466cfb9891eea830e788275c81f6bd2293414c6
rtuttle93 <82906399+rtuttle93@users.noreply.github.com>
2021-08-01 07:40:10 -0400

    Add files via upload
    initial

---
+++ Engineer.js
@@ -0,0 +1,20 @@
+// Engineer Passing Code
+
+const Employee = require('../lib/Employee.js')
+
+class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    this.github = github;
+  }
+
+  getGithub() {
+    return this.github;
+  }
+
+  getRole() {
+    return 'Engineer';
+  }
+}
+
+module.exports = Engineer;
---
+++ Intern.js
@@ -0,0 +1,20 @@
+// Intern Passing Code
+
+const Employee = require('./employee');
+
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber) {
+    super(name, id, email);
+    this.officeNumber = officeNumber;
+  }
+
+  getOfficeNumber() {
+    return this.officeNumber;
+  }
+
+  getRole() {
+    return 'Manager';
+  }
+}
+
+module.exports = Manager;
